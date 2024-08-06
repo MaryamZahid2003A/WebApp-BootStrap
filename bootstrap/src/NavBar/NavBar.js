883be4422385f8ec7ee55d66aa2dcd3fecf0f5b8
Maryam Zahid <maryamzahid960@gmail.com>
2024-08-06 11:48:57 -0700

    commit

---
+++ bootstrap/src/NavBar/NavBar.js
@@ -0,0 +1,30 @@
+import React from 'react'
+import '../App.css'; 
+import Section from './Section';
+export default function NavBar() {
+  return (
+    <div>
+      <nav className='navbar navbar-expand-lg navbar-light bg-black '>
+        <div className='container-fluid'>
+            <a className='navbar-brand text-lg-start text-white '><strong className='fs-4 text-warning'>Frontend BootCamp</strong></a>
+            <button 
+                className='navbar-toggler'
+                type='button' 
+                data-bs-toggle='collapse'
+                data-bs-target='#navbar' 
+              ><span className='fas fa-bars text-warning'></span>
+            </button>
+            <div className='collapse navbar-collapse' id='navbar'>
+                <ul className='navbar-nav ms-auto'>
+                    <li className='nav-item me-3 fs-5 hover-effect active text-white '><a className='nav-link hover-effect text-white'>What You'll Learn</a></li>
+                    <li className='nav-item me-3 fs-5 hover-effect active text-white '><a className='nav-link hover-effect text-white'>Questions</a></li>
+                    <li className='nav-item me-3 fs-5 hover-effect active text-white'><a className='nav-link hover-effect text-white'>Your Instructor</a></li>
+                </ul>
+            </div>
+            
+            </div>
+      </nav>
+      <Section/>
+    </div>
+  )
+}
---
+++ bootstrap/src/NavBar/Section.js
@@ -0,0 +1,29 @@
+import React from 'react';
+
+export default function Section() {
+  return (
+    <div className='bg-black py-4'>
+      <div className='container-fluid d-flex align-items-center justify-content-evenly p-5'>
+        <div>
+          <h1 className='fs-2 text-white text-center'>
+            Become a <span className='text-warning'>Web Developer</span>
+          </h1>
+          <div className='align-items-center p-4'>
+            <p className='text-center text-white'>
+              Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy
+              text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.
+              It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged . . .
+              
+            </p>
+          </div>
+        </div>
+        <img
+          src='https://img.freepik.com/premium-vector/young-man-programmer-working-computer-with-code-screen-student-programming-vector-concept_53562-4999.jpg'
+          className='img-fluid d-none d-sm-block'
+          height='400'
+          width='400'
+        />
+      </div>
+    </div>
+  );
+}
